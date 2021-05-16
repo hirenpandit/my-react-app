e06b26b9f964a5fe7d6085a0793e5c088684e0dd
hiren pandit <hirenppandit@gmail.com>
2021-05-16 19:26:17 +0530

    refactoring

--- src/company/add-company.tsx
+++ src/company/add-company.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default class AddCompany extends React.Component<any, any> {
     
     fakeId = 124;
     state = {
         company: {
-            id: 0,
-            name: "",
-            city: "",
-            region: "" 
+            id : 0,
+            name : "",
+            city : "",
+            region : ""
         }
     }
     addNewCompany = (e:any) => {
@@ -29,31 +30,37 @@ export default class AddCompany extends React.Component<any, any> {
         })
     }
     render() {
-        return <Form onSubmit={this.addNewCompany}>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Company</Form.Label>
-                        <Form.Control type="text" placeholder="Company Name" value={this.state.company.name} onChange={(e)=>{
-                            console.log(e.target.value);
-                            this.setState((prevState:any) => ({company: {...prevState.company, name: e.target.value}}))
-                        }}/>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control type="text" placeholder="City" value={this.state.company.city} onChange={(e)=>{
-                            console.log(e.target.value);
-                            this.setState((prevState:any) => ({company: {...prevState.company, city: e.target.value}}))
-                        }}/>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>State</Form.Label>
-                        <Form.Control type="text" placeholder="State" value={this.state.company.region} onChange={(e)=>{
-                            console.log(e.target.value);
-                            this.setState((prevState:any) => ({company: {...prevState.company, region: e.target.value}}))
-                        }}/>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Add New
-                    </Button>
-                </Form>
+        return <Container>
+            <Row>
+                <Col sm={4}>
+                    <Form onSubmit={this.addNewCompany}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Company</Form.Label>
+                            <Form.Control type="text" placeholder="Company Name" value={this.state.company.name} onChange={(e)=>{
+                                console.log(e.target.value);
+                                this.setState((prevState:any) => ({company: {...prevState.company, name: e.target.value}}))
+                            }}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control type="text" placeholder="City" value={this.state.company.city} onChange={(e)=>{
+                                console.log(e.target.value);
+                                this.setState((prevState:any) => ({company: {...prevState.company, city: e.target.value}}))
+                            }}/>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control type="text" placeholder="State" value={this.state.company.region} onChange={(e)=>{
+                                console.log(e.target.value);
+                                this.setState((prevState:any) => ({company: {...prevState.company, region: e.target.value}}))
+                            }}/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Add New
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     }
 }
